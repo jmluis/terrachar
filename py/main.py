e175fd7f2375fd5ea48c8d6a3f5e5577e48db07c
Jose Luis <shoot@chbshoot.me>
2017-02-02 02:57:07 -0500

    mmm no
    player loading should not be in the player file

--- py/main.py
+++ py/main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import Image
+import json
 from player import Player
+from palette import Palette
 
 def drawCharacter(player, image):
     for piece in player.PALETTE:
@@ -11,11 +13,39 @@ def drawCharacter(player, image):
 
     return image
 
-def main():
-    pl = Player.loadCharacter('0')
+def main(name):
+    f = open("{0}.json".format(name)).read()
+    parsed = json.loads(f)
+    char = Player()
+    char.EYE_COLOR = [int(x) for x in parsed['EyeColor'].split(',')]
+    char.HAIR_COLOR = [int(x) for x in parsed['HairColor'].split(',')]
+    char.PANTS_COLOR = [int(x) for x in parsed['PantsColor'].split(',')]
+    char.SHIRT_COLOR = [int(x) for x in parsed['ShirtColor'].split(',')]
+    char.SHOE_COLOR = [int(x) for x in parsed['ShoeColor'].split(',')]
+    char.SKIN_COLOR = [int(x) for x in parsed['SkinColor'].split(',')]
+    char.UNDERSHIRT_COLOR = [int(x) for x in parsed['UnderShirtColor'].split(',')]
+    char.HAIR = int(parsed['Hair'])
+    char.SKIN_VARIANT = int(parsed['SkinVariant'])
+    char.HEAD_SLOT = int(parsed['HeadSlot'])
+    char.BODY_SLOT = int(parsed['BodySlot'])
+    char.LEGS_SLOT = int(parsed['LegsSlot'])
+    char.HAND_ON_SLOT = int(parsed['HandsOnSlot'])
+    char.HAND_OFF_SLOT = int(parsed['HandsOffSlot'])
+    char.BACK_SLOT = int(parsed['BackSlot'])
+    char.FRONT_SLOT = int(parsed['FrontSlot'])
+    char.SHOE_SLOT = int(parsed['ShoeSlot'])
+    char.WAIST_SLOT = int(parsed['WaistSlot'])
+    char.WING_SLOT = int(parsed['WingSlot'])
+    char.SHIELD_SLOT = int(parsed['ShieldSlot'])
+    char.NECK_SLOT = int(parsed['NeckSlot'])
+    char.FACE_SLOT = int(parsed['FaceSlot'])
+    char.BALLOON_SLOT = int(parsed['BalloonSlot'])
+
+    char.NAME = parsed['Name']
+    char.PALETTE = Palette.load_pieces(char)
 
     char_img = Image.new('RGBA', (40, 55), (255, 255, 255, 255))
-    char_img = drawCharacter(pl, char_img)
+    char_img = drawCharacter(char, char_img)
 
-    char_img.save("{0}.png".format(pl.NAME), "PNG")
-main()
+    char_img.save("{0}.png".format(char.NAME), "PNG")
+main('0')
--- py/player.py
+++ py/player.py
@@ -28,40 +28,8 @@ class Player:
     NECK_SLOT = None
     FACE_SLOT = None
     BALLOON_SLOT = None
+
+
     @property
     def GENDER(self):
         return (self.SKIN_VARIANT > 3 and self.SKIN_VARIANT is not 8)
-
-    @staticmethod
-    def loadCharacter(name):
-        f = open("{0}.json".format(name)).read()
-        parsed = json.loads(f)
-        char = Player()
-        char.EYE_COLOR = [int(x) for x in parsed['EyeColor'].split(',')]
-        char.HAIR_COLOR = [int(x) for x in parsed['HairColor'].split(',')]
-        char.PANTS_COLOR = [int(x) for x in parsed['PantsColor'].split(',')]
-        char.SHIRT_COLOR = [int(x) for x in parsed['ShirtColor'].split(',')]
-        char.SHOE_COLOR = [int(x) for x in parsed['ShoeColor'].split(',')]
-        char.SKIN_COLOR = [int(x) for x in parsed['SkinColor'].split(',')]
-        char.UNDERSHIRT_COLOR = [int(x) for x in parsed['UnderShirtColor'].split(',')]
-        char.HAIR = int(parsed['Hair'])
-        char.SKIN_VARIANT = int(parsed['SkinVariant'])
-        char.HEAD_SLOT = int(parsed['HeadSlot'])
-        char.BODY_SLOT = int(parsed['BodySlot'])
-        char.LEGS_SLOT = int(parsed['LegsSlot'])
-        char.HAND_ON_SLOT = int(parsed['HandsOnSlot'])
-        char.HAND_OFF_SLOT = int(parsed['HandsOffSlot'])
-        char.BACK_SLOT = int(parsed['BackSlot'])
-        char.FRONT_SLOT = int(parsed['FrontSlot'])
-        char.SHOE_SLOT = int(parsed['ShoeSlot'])
-        char.WAIST_SLOT = int(parsed['WaistSlot'])
-        char.WING_SLOT = int(parsed['WingSlot'])
-        char.SHIELD_SLOT = int(parsed['ShieldSlot'])
-        char.NECK_SLOT = int(parsed['NeckSlot'])
-        char.FACE_SLOT = int(parsed['FaceSlot'])
-        char.BALLOON_SLOT = int(parsed['BalloonSlot'])
-
-
-        char.NAME = parsed['Name']
-        char.PALETTE = Palette.load_pieces(char)
-        return char
